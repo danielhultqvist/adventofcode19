21d1ac8eedec6d6247f83e5d199f7aead98f956d
Daniel Hultqvist <daniel.hultqvist@sl.se>
2019-12-02 23:54:48 +0100

    Solve 2b

---
+++ 2/2b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+// Do not make code a reference, it should be copied to "reset" memory
+int calculate(vector<int> code, int noun, int verb) {
+  code[1] = noun;
+  code[2] = verb;
+
+  int position = 0;
+  while (code[position] != 99) {
+    if (code[position] == 1) {
+      code[code[position + 3]] = code[code[position + 1]] + code[code[position + 2]];
+    } else {
+      code[code[position + 3]] = code[code[position + 1]] * code[code[position + 2]];
+    }
+    position += 4;
+  }
+  return code[0];
+}
+
+int main() {
+  ifstream input{"input.txt"};
+  if (!input) {
+    throw runtime_error("Unable to load data");
+  }
+
+  vector<int> code;
+  std::string value;
+  while (std::getline(input, value, ',')) {
+    code.push_back(stol(value));
+  }
+
+  // Extra instructions
+  for (int noun = 0; noun < 100; ++noun) {
+    for (int verb = 0; verb < 100; ++verb) {
+      int result = calculate(code, noun, verb);
+      if (result == 19690720) {
+        cout << "Matching candidate, noun: " << noun << ", verb: " << verb << endl;
+        cout << "Value to enter: " << ((100*noun)+verb) << endl;
+      }
+    }
+  }
+}
+
